ba54abd8d086ec8e0cedc61a2b1f27c4066d5ac2
Thanappu <134472046+Thanappu@users.noreply.github.com>
2024-03-13 22:58:37 +0700

    update

---
+++ test_mesh_lite/main/main.hpp
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+
+#define CONFIG_ROUTER_SSID "SCG-indoor-localize"
+#define CONFIG_ROUTER_PASSWORD "12345678#"
+#define CONFIG_SERVER_IP "192.168.68.113"
+#define CONFIG_SERVER_PORT 8070 
+#define CONFIG_BRIDGE_SOFTAP_SSID "ESP_Bridge"
+#define CONFIG_BRIDGE_SOFTAP_PASSWORD "12345678"
+
+#endif 
